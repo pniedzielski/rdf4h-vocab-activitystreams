8d15316a0f6cf57a632c30d22cae211cabc132b0
Patrick M. Niedzielski <patrick@pniedzielski.net>
2024-04-01 04:27:09 +0000

    Add simple module-level documentation
    Signed-off-by: Patrick M. Niedzielski <patrick@pniedzielski.net>
    License: BSD-3-Clause

--- src/Data/RDF/Vocabulary/ActivityStreams2.hs
+++ src/Data/RDF/Vocabulary/ActivityStreams2.hs
@@ -3,6 +3,20 @@
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 {-# OPTIONS_GHC -Wno-missing-export-lists #-}
 
+-------------------------------------------------------------------------------
+-- |
+-- Module      : Data.RDF.Vocabulary.ActivityStreams2
+-- Description : An RDF vocabulary for ActivityStreams 2.
+-- Copyright   : © 2024, Patrick M. Niedzielski
+-- License     : BSD-3-Clause
+--
+-- Maintainer  : patrick@pniedzielski.net
+-- Stability   : stable
+-- Portability : portable
+--
+-- The ActivityStreams 2 Vocabulary is a foundational RDF vocabulary
+-- for representing activity structures and specific activity types in
+-- a decentralized social media application.
 module Data.RDF.Vocabulary.ActivityStreams2 where
 
 import Data.RDF.Namespace qualified (mkPrefixedNS)
